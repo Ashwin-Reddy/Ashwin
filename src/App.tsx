17a53173fc1cb3b62a466bee804e2095d16cb2a9
Ashwin-Reddy <princeashwin_2248@outlook.com>
2026-09-21 17:21:18 +0530

    Added Splash cursor effect

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import DevStats from "./sections/DevStats";
 import Footer from "./components/Footer";
+import SplashCursor from "./components/SplashCursor";
 
 function App() {
   return (
     <main>
+
+      <SplashCursor
+        DYE_RESOLUTION={1024}
+        SPLAT_RADIUS={0.16}
+        COLOR_UPDATE_SPEED={8}
+        RAINBOW_MODE={false}
+        COLOR="#64FFDD"
+      />
 
       <Navbar />
 
